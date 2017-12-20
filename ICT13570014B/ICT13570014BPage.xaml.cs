c6a0707bfa7dcf3ec7a59f61f6f67a172be4e910
jeeraporn Saitanu <jeerapornsaitanu@MBPkhxneeraporn.true.true>
2017-12-21 01:07:38 +0700

    ส่งงานที่ 1

---
+++ ICT13570014B/ICT13570014BPage.xaml.cs
@@ -0,0 +1,35 @@
+﻿using Xamarin.Forms;
+
+namespace ICT13570014B
+{
+    public partial class ICT13570014BPage : ContentPage
+    {
+        public ICT13570014BPage()
+        {
+            InitializeComponent();
+            okButton.Clicked += OkButton_Clicked;
+            genderPicker.Items.Add("ชาย");
+            genderPicker.Items.Add("หญิง");
+
+            mySlider.ValueChanged += MySlider_ValueChanged;
+            myStepper.ValueChanged += MyStepper_ValueChanged;
+        }
+
+        void OkButton_Clicked(object sender, System.EventArgs e)
+        {
+            CamelCaseLabel.Text = CamelCaseLabel2.Text;
+        }
+
+        void MySlider_ValueChanged(object sender, ValueChangedEventArgs e)
+        {
+            int value = (int)e.NewValue;
+            valueLabel.Text = value.ToString();
+        }
+
+        void MyStepper_ValueChanged(object sender, ValueChangedEventArgs e)
+        {
+            int value = (int)e.NewValue;
+            valueLabel.Text = value.ToString();
+        }
+    }
+}
